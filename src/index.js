61015ad861c78e46466e6a84a6cb1e5826129e9a
SaltyJester <01youngdc@gmail.com>
2021-05-14 00:53:08 -0700

    Added npm discord.js. Playing around with it.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 const clashApi = require('./utils/clash-api');
+const discord = require('discord.js');
+const bot = new discord.Client();
 
-clashApi.getClanInfo(process.env.CLAN_TAG).then((result) => {
-    console.log(result);
-}).catch((e) => {
-    console.log(e);
+const botToken = process.env.BOT_TOKEN;
+
+bot.login(botToken);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+  });
+
+bot.on('message', msg => {
+if (msg.content === 'ping') {
+    msg.reply('pong');
+    msg.channel.send('pong');
+}
 });
+
+// clashApi.getClanInfo(process.env.CLAN_TAG).then((result) => {
+//     console.log(result);
+// }).catch((e) => {
+//     console.log(e);
+// });
