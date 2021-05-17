48787c7077ad023f798f59a4e8d809e5c256d2fa
SaltyJester <01youngdc@gmail.com>
2021-05-17 00:05:21 -0700

    learning how send messages through bot

--- src/index.js
+++ src/index.js
@@ -1,24 +1,49 @@
 const clashApi = require('./utils/clash-api');
-// const discord = require('discord.js');
-// const bot = new discord.Client();
+const discord = require('discord.js');
+const client = new discord.Client();
 
-// const botToken = process.env.BOT_TOKEN;
+const botToken = process.env.BOT_TOKEN;
 
-// bot.login(botToken);
+client.login(botToken);
 
-// bot.on('ready', () => {
-//     console.info(`Logged in as ${bot.user.tag}!`);
-//   });
+client.once('ready', () => {
+    // console.log(`Logged in as ${client.user.tag}!`);
+  });
 
-// bot.on('message', msg => {
-// if (msg.content === 'ping') {
-//     msg.reply('pong');
-//     msg.channel.send('pong');
-// }
-// });
-
-clashApi.getClanInfo(process.env.CLAN_TAG).then((result) => {
-    console.log(result);
-}).catch((e) => {
-    console.log(e);
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+        msg.channel.send('pong');
+    }
 });
+
+// const date = new Date();
+// console.log(date.toISOString());
+
+client.on('message', msg => {
+    let state = {
+        remind: false
+    }
+    if(msg.content === '!remind'){
+        state.remind = true;
+        try{
+            const requestLoop = setInterval(async () => {
+                const response = await clashApi.getCurrentWar(process.env.CLAN_TAG);
+                msg.channel.send(response.data.state);
+            }, 10000);
+        }catch(e){
+            msg.channel.send('error has occured');
+        }
+        console.log('I can still continue');
+    }else if(msg.content === "!status"){
+        msg.channel.send('Status is ' + state.remind);
+    }
+});
+
+// const requestLoop = setInterval(async () => {
+//     const response = await clashApi.getCurrentWar(process.env.CLAN_TAG);
+//     if(response.data.state === 'warEnded'){
+//         console.log(response.data.state);
+//         msg.channel(response.data.state);
+//     }
+// }, 1000);
--- tests/clash-api.test.js
+++ tests/clash-api.test.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
-const MockAdapter = require('axios-mock-adapter');
+// const MockAdapter = require('axios-mock-adapter');
 const clashApi = require('../src/utils/clash-api');
 
 test('test', async () => {
-    const mock = new MockAdapter(axios);
-    const data = { response: 'hello there' };
-    mock.onGet('https://api.clashofclans.com/v1/clans/' + process.env.CLAN_TAG).reply(200, data);
+    // const mock = new MockAdapter(axios);
+    // const data = { response: 'hello there' };
+    // mock.onGet('https://api.clashofclans.com/v1/clans/' + process.env.CLAN_TAG).reply(200, data);
 
     const response = await clashApi.getClanInfo(process.env.CLAN_TAG);
-    console.log(response)
+    // console.log(response)
     expect(response.status).toBe(200);
 });
